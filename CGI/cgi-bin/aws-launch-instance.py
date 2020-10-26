bd1b6f0f05442b5a91e33b59dbb85db026f57ae7
muskan399 <63671368+muskan399@users.noreply.github.com>
2020-10-26 21:30:58 +0530

    Update aws-launch-instance.py

--- CGI/cgi-bin/aws-launch-instance.py
+++ CGI/cgi-bin/aws-launch-instance.py
@@ -15,9 +15,8 @@ elif(image=="amazon linux2"):
 	ami="ami-0e306788ff2473ccb"
 else:
 	ami="ami-03cfb5e1fb4fac428"
-instance_type="t2.micro"
 
-cmd="AWS_ACCESS_KEY_ID=AKIA26T7HQNFTLXTSX7T AWS_SECRET_ACCESS_KEY=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 aws ec2 run-instances --image-id {0} --count 1 --instance-type {1} --security-group-ids sg-00b59370d35349eaa --region ap-south-1".format(ami,instance_type)
+cmd="AWS_ACCESS_KEY_ID=***** AWS_SECRET_ACCESS_KEY=***** aws ec2 run-instances --image-id {0} --count 1 --instance-type {1} --security-group-ids sg-00b59370d35349eaa --region ap-south-1".format(ami,instance_type)
 cmd_out=subprocess.getstatusoutput(cmd)
 status_code=cmd_out[0]
 output=cmd_out[1]
